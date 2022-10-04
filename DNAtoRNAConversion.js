be70588adffd0bb15ee7a6ebeef6cc261af7e84b
Han Ye Htun <108056141+sloth30799@users.noreply.github.com>
2022-10-04 13:29:58 +0630

    Create DNAtoRNAConversion.js

---
+++ DNAtoRNAConversion.js
@@ -0,0 +1,16 @@
+// Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+// Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+// Create a function which translates a given DNA string into RNA.
+
+function DNAtoRNA(dna) {
+  // create a function which returns an RNA sequence from the given DNA sequence
+  dna = dna.split('')
+  for (let i = 0; i < dna.length; i++) {
+    if (dna[i] === "T") {
+      dna[i] = "U"
+    }
+  }
+  return dna.join('')
+}
