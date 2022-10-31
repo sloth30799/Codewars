820626affdd6f3a280d7e88937c0898c4ebf8d03
Han Ye Htun <sloth30799@gmail.com>
2022-10-31 13:24:55 +0630

    return largest string

---
+++ calculateAverage.js
@@ -0,0 +1,15 @@
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+//  code
+function findAverage(array) {
+    let length = array.length
+    
+    if (length <= 0) {
+      return 0
+    } else {
+      array = array.reduce((total, x) => total + x, 0)
+      return array / length
+    }
+  }
