450092acb1650cc19d4f5d297d6d38015f8493da
Han Ye Htun <sloth30799@gmail.com>
2022-11-11 10:21:56 +0630

    return sum

---
+++ sumOfNumbers3.js
@@ -0,0 +1,22 @@
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+// code
+const getSum = (a,b) => {
+    let sum = 0
+    let i = Math.min(a,b)
+    let j = Math.max(a,b)
+    for (i; i <= j;i++) {
+      sum += i
+    }
+    return sum
+  }
