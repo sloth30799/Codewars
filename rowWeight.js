2f8d996ea15336aa3f9b20676d2cc72accfd61e4
Han Ye Htun <sloth30799@gmail.com>
2022-12-17 14:25:41 +0630

    array practice

---
+++ rowWeight.js
@@ -0,0 +1,25 @@
+// Scenario
+// Several people are standing in a row divided into two teams.
+// The first person goes into team 1, the second goes into team 2, the third goes into team 1, and so on.
+
+// Task
+// Given an array of positive integers (the weights of the people), return a new array/tuple of two integers, where the first one is the total weight of team 1, and the second one is the total weight of team 2.
+
+// Notes
+// Array size is at least 1.
+// All numbers will be positive.
+// Input >> Output Examples
+// rowWeights([13, 27, 49])  ==>  return (62, 27)
+// Explanation:
+// The first element 62 is the total weight of team 1, and the second element 27 is the total weight of team 2.
+
+function rowWeights(array){
+    return array.reduce(([fir, sec], x, index) => {
+      if (index % 2 === 0) {
+        fir += x
+      } else {
+        sec += x
+      }
+      return [fir, sec]
+    }, [0, 0])
+  }
