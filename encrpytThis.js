0c264ffbfc9d427b65a1c5c60bb2609fd67ce7d1
Han Ye Htun <sloth30799@gmail.com>
2022-12-28 19:37:59 +0630

    encrypting

---
+++ encrpytThis.js
@@ -0,0 +1,29 @@
+// Acknowledgments:
+// I thank yvonne-liu for the idea and for the example tests :)
+
+// Description:
+// Encrypt this!
+
+// You want to create secret messages which can be deciphered by the Decipher this! kata. Here are the conditions:
+
+// Your message is a string containing space separated words.
+// You need to encrypt each word in the message using the following rules:
+// The first letter must be converted to its ASCII code.
+// The second letter must be switched with the last letter
+// Keepin' it simple: There are no special characters in the input.
+// Examples:
+// encryptThis("Hello") === "72olle"
+// encryptThis("good") === "103doo"
+// encryptThis("hello world") === "104olle 119drlo"
+
+var encryptThis = function(text) {
+    return text.split(' ').map(word => {
+      if (word.length < 2) {
+        return word.charCodeAt()
+      } else if (word.length < 3) {
+        return [word.charCodeAt(), word.slice(1).split('').reverse().join('')].join('')
+      } else {
+        return [word.charCodeAt(), word[word.length - 1], word.slice(2, -1), word[1]].join('')
+      }
+    }).join(' ')
+  }
