9a33c171bfef6e9a3fbbfe3624a43e1e871b4003
Han Ye Htun <sloth30799@gmail.com>
2023-01-24 20:08:29 +0630

    find maximum product

---
+++ maximumProduct.js
@@ -0,0 +1,10 @@
+// Task
+// Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers in the array.
+
+function adjacentElementsProduct(array) {
+    let newArr = []
+    for (let i = 0; i < array.length - 1; i++) {
+      newArr.push(array[i] * array[i + 1])
+    }
+    return Math.max(...newArr)
+  }
