66f75ff620eb0380ca30e2cd9933f1a06af68444
Han Ye Htun <sloth30799@gmail.com>
2023-09-27 21:29:55 +0630

    remove vowels

---
+++ Python/vowelRemover.py
@@ -0,0 +1,20 @@
+# Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+# Examples
+# "hello"     -->  "hll"
+# "codewars"  -->  "cdwrs"
+# "goodbye"   -->  "gdby"
+# "HELLO"     -->  "HELLO"
+# don't worry about uppercase vowels
+# y is not considered a vowel for this kata
+
+def shortcut(s):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+
+    s = list(s)
+    result = list(filter(lambda x: x not in vowels, s))
+
+    return ''.join(result)
+
+
+print(shortcut("hello"))
