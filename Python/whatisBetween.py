2e43f16ca72f70f4b424e26e9ff0b8766f4e9c0f
Han Ye Htun <sloth30799@gmail.com>
2023-09-27 21:06:43 +0630

    make a list of number in between

---
+++ Python/whatisBetween.py
@@ -0,0 +1,21 @@
+# Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+# For example:
+
+# a = 1
+# b = 4
+# --> [1, 2, 3, 4]
+
+#  my solution
+def between(a, b):
+    arr = []
+    for i in range(a, b + 1):
+        arr.append(i)
+
+    return arr
+
+# best
+
+
+def between(a, b):
+    return list(range(a, b+1))
